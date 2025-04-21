2aae66362509c8468d5d590abb1e8ead1b31cd7d
Borja Sevilla <bsevrui@gmail.com>
2025-04-21 17:38:53 +0200

    chore(icons): updated list icon

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -2,7 +2,7 @@ import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
 import { addIcons } from 'ionicons';
-import { home, list, person } from 'ionicons/icons';
+import { home, listSharp, person } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -14,6 +14,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ home, list, person });
+    addIcons({ home, listSharp, person });
   }
 }
