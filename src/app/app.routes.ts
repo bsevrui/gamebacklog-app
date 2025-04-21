4ad5d40e7cf593c07a4aefc117e27035d68d23df
Borja Sevilla <bsevrui@gmail.com>
2025-04-21 18:38:20 +0200

    feat(about): added about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./pages/tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about.page').then( m => m.AboutPage)
+  },
 ];
---
+++ src/app/pages/about/about.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { DeviceService } from 'src/app/core/services/device.service';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.page.html',
+  styleUrls: ['./about.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TranslateModule, IonItem, IonLabel]
+})
+export class AboutPage implements OnInit {
+  /* Device's Model */
+  public deviceModel$: string = 'undefined';
+
+  /* Device's UUID */
+  public deviceUUID$: string = 'undefined';
+
+  /* Device's OS */
+  public deviceOS$: string = 'undefined';
+
+  /**
+   * Constructor
+   * @param deviceService   Device Service.
+   */
+  constructor(
+    private deviceService: DeviceService
+  ) {}
+
+  ngOnInit() {
+    this._getDeviceData();
+  }
+
+  ionViewWillEnter() {
+    this._getDeviceData();
+  }
+
+  /**
+   * Get the Device's information
+   * @returns void.
+   */
+  private _getDeviceData(): void {
+    this.deviceModel$ = this.deviceService.deviceModel();
+    this.deviceUUID$ = this.deviceService.deviceUUID();
+    this.deviceOS$ = this.deviceService.deviceOSVersion();
+  }
+}
