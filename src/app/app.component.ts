922140a1a0ce8363075c2e0968047080e2b63f3e
Borja Sevilla <bsevrui@gmail.com>
2025-04-21 18:27:51 +0200

    feat(app): added initializeDevice() on start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@capacitor/splash-screen';
 import { LocalizationService } from './core/services/localization.service';
+import { DeviceService } from './core/services/device.service';
 
 @Component({
   selector: 'app-root',
@@ -13,16 +14,20 @@ export class AppComponent {
   /**
    * Constructor
    * @param platform platform     Platform.
+   * @param localizationService   Localization Service.
+   * @param deviceService         Device Service.
    */
   constructor(
     private platform: Platform,
-    private localizationService: LocalizationService
+    private localizationService: LocalizationService,
+    private deviceService: DeviceService
   ) {
     this.initializeApp();
   }
 
   public initializeApp() {
     this.localizationService.initializeLocalization();
+    this.deviceService.initializeDevice();
     this.platform.ready().then(async () => {
       await SplashScreen.hide();
     });
