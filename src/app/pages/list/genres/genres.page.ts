d51a6c246e4cbff5507b18a4318c992f4e5e8968
Borja Sevilla <bsevrui@gmail.com>
2025-04-22 13:37:17 +0200

    feat(menu/multilang): added menu & multi lang on genres page

--- src/app/pages/list/genres/genres.page.ts
+++ src/app/pages/list/genres/genres.page.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-genres',
   templateUrl: './genres.page.html',
   styleUrls: ['./genres.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TranslateModule, IonButtons, IonMenuButton]
 })
 export class GenresPage implements OnInit {
+  /* Flag for the limit of generes load on each interaction */
+  limit: number = 15;
+  /* Flag for the start of the array */
+  start: number = 0;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ionViewDidEnter() {
+    this.start = 0;
   }
 
 }
