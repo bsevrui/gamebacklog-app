23333cca34f91dafe4eb5cbae68ad5faf7831b81
Borja Sevilla <bsevrui@gmail.com>
2025-04-22 13:42:47 +0200

    feat(menu/multilang): added menu & multilang on platforms page

--- src/app/pages/list/platforms/platforms.page.ts
+++ src/app/pages/list/platforms/platforms.page.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-platforms',
   templateUrl: './platforms.page.html',
   styleUrls: ['./platforms.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, TranslateModule]
 })
 export class PlatformsPage implements OnInit {
+  /* Flag for the limit of platforms load on each interaction */
+  limit: number = 15;
+  /* Flag for the start of the array */
+  start: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ionViewDidEnter() {
+    this.start = 0;
   }
-
 }
