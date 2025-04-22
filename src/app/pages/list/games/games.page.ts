791a3835f8f6f0f71a362e2e7c6e53388fa37eb1
Borja Sevilla <bsevrui@gmail.com>
2025-04-22 13:32:16 +0200

    feat(menu/multilang): added menu button & multilang on games page

--- src/app/pages/list/games/games.page.ts
+++ src/app/pages/list/games/games.page.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-games',
   templateUrl: './games.page.html',
   styleUrls: ['./games.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, TranslateModule]
 })
 export class GamesPage implements OnInit {
+  /* Flag for the limit of games load on each interaction */
+  limit: number = 15;
+  /* Flag for the start of the array */
+  start: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ionViewDidEnter() {
+    this.start = 0;
   }
-
 }
