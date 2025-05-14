81d2334edf1ec62eaea41892c7781011b06c841f
Borja Sevilla <bsevrui@gmail.com>
2025-05-14 18:23:34 +0200

    feat(enviroronment): added const

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apiHostUrl: 'http://localhost/api/',
+  gamesEndpoint: 'http://localhost/api/games/',
+  genresEndpoint: 'http://localhost/api/genres/',
+  platformsEndpoint: 'http://localhost/api/platforms/',
+  usersEndpoint: 'http://localhost/api/users/'
 };
