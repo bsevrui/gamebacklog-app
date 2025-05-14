d94126363ec5d5d215de829b8e7479105abb7dcf
Borja Sevilla <bsevrui@gmail.com>
2025-05-14 23:03:30 +0200

    refactor(environment): removed unused code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: true,
-  apiHostUrl: 'http://localhost/api/',
-  gamesEndpoint: 'http://localhost/api/games/',
-  genresEndpoint: 'http://localhost/api/genres/',
-  platformsEndpoint: 'http://localhost/api/platforms/',
-  usersEndpoint: 'http://localhost/api/users/'
+  apiHostUrl: 'http://localhost/api/'
 };
