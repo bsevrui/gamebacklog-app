dd4738a3fdab99e8a5a6aa4c4690e9772603d9e3
Borja Sevilla <bsevrui@gmail.com>
2025-05-20 19:21:50 +0200

    feat(games): added list functionality

--- src/app/pages/list/games/games.page.ts
+++ src/app/pages/list/games/games.page.ts
@@ -1,27 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonList, IonItem, IonLabel, IonRouterLink, IonThumbnail } from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
+import { Game } from 'src/app/core/interfaces/game';
+import { ApiService } from 'src/app/core/services/api.service';
 
 @Component({
   selector: 'app-games',
   templateUrl: './games.page.html',
   styleUrls: ['./games.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, TranslateModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, TranslateModule, IonList, IonItem, IonLabel, IonRouterLink, IonThumbnail]
 })
 export class GamesPage implements OnInit {
-  /* Flag for the limit of games load on each interaction */
-  limit: number = 15;
-  /* Flag for the start of the array */
-  start: number = 0;
+  /* Flag for the games' array */
+  games: Game[] = [];
 
-  constructor() {}
+  /**
+   * Constructor
+   * @param apiService    API Service
+   */
+  constructor(private apiService: ApiService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loadData();
+  }
 
-  ionViewDidEnter() {
-    this.start = 0;
+  /**
+   * Loads games from db
+   */
+  loadData() {
+    this.apiService.getGames().subscribe(
+      (data) => {
+        this.games = data;
+      }
+    );
   }
 }
