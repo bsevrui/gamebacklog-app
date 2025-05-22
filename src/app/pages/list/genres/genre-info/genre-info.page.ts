80c6a19c93f361f0ad3e68a03cff373dd0718446
Borja Sevilla <bsevrui@gmail.com>
2025-05-22 20:52:55 +0200

    feat(genres-info): completed functionality

--- src/app/pages/list/genres/genre-info/genre-info.page.ts
+++ src/app/pages/list/genres/genre-info/genre-info.page.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonList, IonItem, IonThumbnail, IonLabel } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/core/services/api.service';
-import { Game } from 'src/app/core/interfaces/game';
 import { Genre } from 'src/app/core/interfaces/genre';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-genre-info',
   templateUrl: './genre-info.page.html',
   styleUrls: ['./genre-info.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonList, IonItem, IonThumbnail, IonLabel, RouterLink]
 })
 export class GenreInfoPage implements OnInit {
   private genreId: number = 0;
