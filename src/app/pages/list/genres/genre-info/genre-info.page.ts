b8e57dc8beb81c9a329d5711ca8dac97df7bb9c7
Borja Sevilla <bsevrui@gmail.com>
2025-05-22 20:46:24 +0200

    feat(genre-info): working on

--- src/app/pages/list/genres/genre-info/genre-info.page.ts
+++ src/app/pages/list/genres/genre-info/genre-info.page.ts
@@ -1,20 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/core/services/api.service';
+import { Game } from 'src/app/core/interfaces/game';
+import { Genre } from 'src/app/core/interfaces/genre';
 
 @Component({
   selector: 'app-genre-info',
   templateUrl: './genre-info.page.html',
   styleUrls: ['./genre-info.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton]
 })
 export class GenreInfoPage implements OnInit {
+  private genreId: number = 0;
+  public genre?: Genre;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private apiService: ApiService
+  ) {}
 
   ngOnInit() {
+    const ID = this.activatedRoute.snapshot.paramMap.get('id');
+    if (ID != null) {
+      this.genreId = parseInt(ID);
+      this.loadData();
+    }
   }
 
+  loadData() {
+    this.apiService.getGenre(this.genreId).subscribe(
+      (data) => {
+        console.log(data);
+        this.genre = data;
+      }
+    );
+  }
 }
