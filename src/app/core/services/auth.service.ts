c34cf19fb3b848ac6818ba33fe6d32974f79bf23
Borja Sevilla <bsevrui@gmail.com>
2025-05-27 19:24:51 +0200

    feat(auth-service): completed

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() {}
+  signup(userData: any): Observable<any> {
+    return this.http.post(environment.apiBaseUrl+"auth/signup", userData);
+  }
+
+  login(credentials: any): Observable<any> {
+    return this.http.post(environment.apiBaseUrl+"auth/login", credentials);
+  }
 }
