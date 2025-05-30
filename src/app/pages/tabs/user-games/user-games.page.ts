6d877b01d90db2416b5264b1df027a6c09acc938
Borja Sevilla <bsevrui@gmail.com>
2025-05-30 22:17:04 +0200

    feat(my list): working on

--- src/app/pages/tabs/user-games/user-games.page.ts
+++ src/app/pages/tabs/user-games/user-games.page.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonSegment, IonSegmentButton, IonSegmentContent, IonSegmentView, IonList, IonListHeader, IonItem, IonThumbnail, IonLabel } from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
+import { Game } from 'src/app/core/interfaces/game';
+import { ApiService } from 'src/app/core/services/api.service';
+import { StorageService } from 'src/app/core/services/storage.service';
 
 @Component({
   selector: 'app-usergames',
   templateUrl: 'user-games.page.html',
   styleUrls: ['user-games.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, TranslateModule, IonButtons, IonMenuButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, TranslateModule, IonButtons, IonMenuButton, IonSegment, IonSegmentButton, IonSegmentContent, IonSegmentView, IonList, IonListHeader, IonItem, IonThumbnail, IonLabel],
 })
-export class UserGamesPage {
-  constructor() {}
+export class UserGamesPage implements OnInit {
+  /* Flag for current user playing games' array */
+  playingGames: Game[] = [];
+  /* Flag for current user completed games' array */
+  completedGames: Game[] = [];
+  /* Flag for current user played games' array */
+  playedGames: Game[] = [];
+  /* Flag for current user on-hold games' array */
+  onHoldGames: Game[] = [];
+  /* Flag for current user plan-to-play games' array */
+  planToPlayGames: Game[] = [];
+  /* Flag for current user dropped games' array */
+  droppedGames: Game[] = [];
+
+  /**
+   * Constructor
+   * @param apiService API Service.
+   * @param storageService Storage Service.
+   */
+  constructor(
+    private apiService: ApiService,
+    private storageService: StorageService
+  ) {}
+
+  ngOnInit(): void {}
 }
