1846e48698aec74dfa9393c460d8be526d23db5e
Borja Sevilla <bsevrui@gmail.com>
2025-05-31 21:08:12 +0200

    feat(user games): working on

--- src/app/pages/tabs/user-games/user-games.page.ts
+++ src/app/pages/tabs/user-games/user-games.page.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonSegment, IonSegmentButton, IonSegmentContent, IonSegmentView, IonList, IonListHeader, IonItem, IonThumbnail, IonLabel } from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
-import { Game } from 'src/app/core/interfaces/game';
 import { ApiService } from 'src/app/core/services/api.service';
 import { StorageService } from 'src/app/core/services/storage.service';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { User } from 'src/app/core/interfaces/user';
+import { UsersGames } from 'src/app/core/interfaces/usersgames';
 
 @Component({
   selector: 'app-usergames',
@@ -15,9 +15,15 @@ import { User } from 'src/app/core/interfaces/user';
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, TranslateModule, IonButtons, IonMenuButton, IonSegment, IonSegmentButton, IonSegmentContent, IonSegmentView, IonList, IonListHeader, IonItem, IonThumbnail, IonLabel, RouterLink, CommonModule],
 })
 export class UserGamesPage implements OnInit {
-  currentUser?: User;
-  currentUserId: number = 0;
-  userGames: Game[] = [];
+  private currentUser?: User;
+  private currentUserId: number = 0;
+  private userGames?: UsersGames[];
+  public playingGames?: UsersGames[];
+  public completedGames?: UsersGames[];
+  public playedGames?: UsersGames[];
+  public onHoldGames?: UsersGames[];
+  public planToPlayGames?: UsersGames[];
+  public droppedGames?: UsersGames[];
 
   /**
    * Constructor
@@ -39,7 +45,14 @@ export class UserGamesPage implements OnInit {
       this.currentUserId = this.currentUser.id;
       this.apiService.getUser(this.currentUserId).subscribe(
         (data) => {
-          console.log(data);
+          this.userGames = data.games;
+          console.log(this.userGames);
+          this.playingGames = this.userGames?.filter(usergame => usergame.status === 'Playing');
+          this.completedGames = this.userGames?.filter(usergame => usergame.status === 'Completed');
+          this.playedGames = this.userGames?.filter(usergame => usergame.status === 'Played');
+          this.onHoldGames = this.userGames?.filter(usergame => usergame.status === 'On-Hold');
+          this.planToPlayGames = this.userGames?.filter(usergame => usergame.status === 'Plan-To-Play');
+          this.droppedGames = this.userGames?.filter(usergame => usergame.status === 'Dropped');
         }
       );
     } else {
