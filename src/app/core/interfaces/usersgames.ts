84e1bd9a0b0ab9c290f6f0ed1620fb5f144205a2
Borja Sevilla <bsevrui@gmail.com>
2025-05-31 20:26:17 +0200

    feat(interfaces): updated usersgames interface

--- src/app/core/interfaces/usersgames.ts
+++ src/app/core/interfaces/usersgames.ts
@@ -1,8 +1,11 @@
+import { Game } from "./game";
+
 export interface UsersGames {
     userId: number;
     gameId: number;
     status: string;
     installed: number;
     platinum: number;
+    game: Game;
     score?: number;
 }
