636603d24270ed6a2eebd55ea3c755d574b54731
Borja Sevilla <bsevrui@gmail.com>
2025-06-02 22:18:10 +0200

    feat(user games/update): working on

--- src/app/pages/tabs/user-games/update/update.page.ts
+++ src/app/pages/tabs/user-games/update/update.page.ts
@@ -1,29 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonList, IonItem, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
 import { ApiService } from 'src/app/core/services/api.service';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { UsersGames } from 'src/app/core/interfaces/usersgames';
 
 @Component({
   selector: 'app-update',
   templateUrl: './update.page.html',
   styleUrls: ['./update.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonList, IonItem, IonSelect, IonSelectOption, TranslateModule]
 })
 export class UpdatePage implements OnInit {
+  public usergame?: UsersGames;
+
   constructor(
     private apiService: ApiService,
     private activatedRoute: ActivatedRoute
   ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
     const userId = this.activatedRoute.snapshot.paramMap.get('userId');
     console.log('userId: ', userId);
     const gamedId = this.activatedRoute.snapshot.paramMap.get('gameId');
     console.log('gameId: ', gamedId);
+    if (userId) {
+      this.apiService.getUser(parseInt(userId)).subscribe(
+        (data) => {
+          console.log(data.games);
+          if (gamedId) {
+            this.usergame = data.games?.find(usergame => usergame.gameId == parseInt(gamedId));
+            console.log(this.usergame);
+          }
+        }
+      );
+    }
   }
 
   /* ejemplo
