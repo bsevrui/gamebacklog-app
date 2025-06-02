2ea514d6c623f7afdd4f636228c3410e80c34fef
Borja Sevilla <bsevrui@gmail.com>
2025-06-02 18:41:01 +0200

    feat(user games): working on update

--- src/app/pages/tabs/user-games/update/update.page.ts
+++ src/app/pages/tabs/user-games/update/update.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { TranslateModule } from '@ngx-translate/core';
+import { ApiService } from 'src/app/core/services/api.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-update',
@@ -11,10 +14,29 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class UpdatePage implements OnInit {
-
-  constructor() { }
+  constructor(
+    private apiService: ApiService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    const userId = this.activatedRoute.snapshot.paramMap.get('userId');
+    console.log('userId: ', userId);
+    const gamedId = this.activatedRoute.snapshot.paramMap.get('gameId');
+    console.log('gameId: ', gamedId);
   }
 
+  /* ejemplo
+  update() {
+    this.apiService.updateUserGame(1, 42, {
+      status: 'Completed',
+      score: 5,
+      installed: true,
+      platinum: true,
+    }).subscribe({
+      next: (res) => console.log('updated: ', res),
+      error: (err) => console.error('error: ', err)
+    });
+  }
+    */
 }
