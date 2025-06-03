0550353d45f87c188518854b677affc32a020aff
Borja Sevilla <bsevrui@gmail.com>
2025-06-03 23:39:23 +0200

    feat(usergame/add): working on

--- src/app/pages/tabs/user-games/add/add.page.ts
+++ src/app/pages/tabs/user-games/add/add.page.ts
@@ -1,18 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonButton, IonList, IonItem, IonSelect, IonSelectOption, IonIcon } from '@ionic/angular/standalone';
+import { TranslateModule } from '@ngx-translate/core';
+import { UsersGames } from 'src/app/core/interfaces/usersgames';
+import { Game } from 'src/app/core/interfaces/game';
+import { ActivatedRoute, RouterLink, Router } from '@angular/router';
+import { ApiService } from 'src/app/core/services/api.service';
+import { addIcons } from 'ionicons';
+import { close, saveSharp } from 'ionicons/icons';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.page.html',
   styleUrls: ['./add.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TranslateModule, IonButtons, IonBackButton, IonButton, IonList, IonItem, IonSelect, IonSelectOption, IonIcon, RouterLink]
 })
 export class AddPage implements OnInit {
-  constructor() {}
+  private userId?: number;
+  private gameId?: number;
+  public game?: Game;
+  public usergame?: UsersGames;
+  public status?: 'Playing' | 'Completed' | 'Played' | 'On-Hold' | 'Plan-To-Play' | 'Dropped';
+  public score?: number;
+  public installed?: boolean;
+  public platinum?: boolean;
 
-  ngOnInit() {}
+  constructor(
+    private apiService: ApiService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    addIcons({ close, saveSharp });
+  }
 
+  ngOnInit() {
+    this.userId = Number(this.activatedRoute.snapshot.paramMap.get('userId'));
+    this.gameId = Number(this.activatedRoute.snapshot.paramMap.get('gameId'));
+    this.loadGameData();
+  }
+
+  loadGameData() {
+    if (this.gameId) {
+      this.apiService.getGame(this.gameId).subscribe(
+        (data) => {
+          this.game = data;
+          console.log(this.game);
+        }
+      );
+    }
+  }
 }
