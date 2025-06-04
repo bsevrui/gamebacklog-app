84235cce7ba75e9b8e41aca0044b60084d4c16c5
Borja Sevilla <bsevrui@gmail.com>
2025-06-04 18:19:58 +0200

    feat(list/platforms): added searchbar

--- src/app/pages/list/platforms/platforms.page.ts
+++ src/app/pages/list/platforms/platforms.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonList, IonItem, IonLabel, IonThumbnail } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonList, IonItem, IonLabel, IonThumbnail, IonSearchbar } from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
 import { Platform } from 'src/app/core/interfaces/platform';
 import { ApiService } from 'src/app/core/services/api.service';
@@ -12,11 +12,15 @@ import { RouterLink } from '@angular/router';
   templateUrl: './platforms.page.html',
   styleUrls: ['./platforms.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, TranslateModule, IonList, IonLabel, IonItem, IonThumbnail, RouterLink]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, TranslateModule, IonList, IonLabel, IonItem, IonThumbnail, RouterLink, IonSearchbar]
 })
 export class PlatformsPage implements OnInit {
   /* Flag for the platforms' array */
-  platforms: Platform[] = [];
+  private platforms: Platform[] = [];
+  /* Flag for search query */
+  public searchQuery: string = "";
+  /* Flag for filtered platforms */
+  public filteredPlatforms: Platform[] = [];
 
   /**
    * Constructor
@@ -35,7 +39,12 @@ export class PlatformsPage implements OnInit {
     this.apiService.getPlatforms().subscribe(
       (data) => {
         this.platforms = data;
+        this.filteredPlatforms = data;
       }
     );
+  }
+
+  filterPlatforms() {
+    this.filteredPlatforms = this.platforms.filter((platform => platform.name.toLowerCase().includes(this.searchQuery.toLocaleLowerCase())));
   }
 }
