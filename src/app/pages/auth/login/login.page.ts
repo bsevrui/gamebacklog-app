df8d6b9515c9e9a831b4a20e195326baa7f431f0
Borja Sevilla <bsevrui@gmail.com>
2025-06-04 19:21:08 +0200

    chore();: rearranged imports

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonInput, IonButtons, IonMenuButton, IonButton } from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonInput, IonButtons, IonMenuButton, IonButton } from '@ionic/angular/standalone';
 import { RouterLink, Router } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { StorageService } from 'src/app/core/services/storage.service';
-import { ToastController } from '@ionic/angular'
 import { LocalizationService } from 'src/app/core/services/localization.service';
+import { ToastController } from '@ionic/angular'
 
 @Component({
   selector: 'app-login',
